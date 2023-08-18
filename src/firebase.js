73416ef53422145422ec5f407bbd16d933f86d24
Lawrence Njoroge <lawlens11@gmail.com>
2023-08-19 00:32:11 +0300

    bug fixes

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtynWs5QZYjmeHqTujFlAC9Jx4Ieu-AKU",
-  authDomain: "tinderclone-9d35b.firebaseapp.com",
-  projectId: "tinderclone-9d35b",
-  storageBucket: "tinderclone-9d35b.appspot.com",
-  messagingSenderId: "188656123005",
-  appId: "1:188656123005:web:188e598d01d518f8dc8514",
-  measurementId: "G-W76T0XHSHV"
+  //your firebase config if need be
 };
 
 const app = initializeApp(firebaseConfig)
